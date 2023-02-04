0f2a88c519d11d3a74037a48ac23d8d69fac5ace
Valovition1 <123547273+Valovition1@users.noreply.github.com>
2023-02-04 16:44:42 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,37 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\src\PHPMailer;
+use PHPMailer\src\SMTP;
+use PHPMailer\src\Exception;
+
+require 'PHPMailer\src\PHPMailer';
+require 'PHPMailer\src\SMTP';
+require 'PHPMailer\src\Exception';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings                     //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'domashop.info@gmail.com';                     //SMTP username
+    $mail->Password   = 'pedzcczelxrjlnuz';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('domashop.info@gmail.com');
+    $mail->addAddress($_POST['email']);     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Doma Shop';
+    $mail->Body    = 'This is the verification code';
+    $mail->send();
+} catch (Exception $e) {
+    //error
+}
